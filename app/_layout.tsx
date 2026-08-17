020d1e1b9e32968668da50ed0167d01586e84182
Milton Munoz <livemilton@gmail.com>
2026-08-16 22:46:36 -0500

    feat((HU-03)-HU(HU-03): :sparkles: Movies App - React Native - axios client and first request

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,12 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 import '../global.css';
+import { nowPlayingAction } from '@/core/actions/movies/now-playing.action';
 
 
 const RootLayout = () => {
+
+  nowPlayingAction();
   return (
     <View>
       <Text className='text-3xl'>RootLayout</Text>
