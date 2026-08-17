74dc9e6d95e3737a1d7ee484a1ce23d1fed4d080
Milton Munoz <livemilton@gmail.com>
2026-08-17 14:47:34 -0500

    feat((HU-03)-HU(HU-03): :sparkles: Movies App - React Native - loading screen and SafeArea

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,29 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+// const { getDefaultConfig } = require("expo/metro-config");
+// const { withNativeWind } = require("nativewind/metro");
 
+// const config = getDefaultConfig(__dirname);
+
+// // Ensure Metro prefers React Native builds before ESM/module fields.
+// config.resolver = config.resolver || {};
+// config.resolver.resolverMainFields = [
+//   "react-native",
+//   "react-native-web",
+//   "browser",
+//   "module",
+//   "main",
+// ];
+
+// module.exports = withNativeWind(config, { input: "../global.css" });
+
+const { getDefaultConfig } = require("expo/metro-config");
+
+/** 
+@type
+ 
+{import('expo/metro-config').MetroConfig}
+ */
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "../global.css" });
+config.resolver.unstable_enablePackageExports = false;
+
+module.exports = config;
