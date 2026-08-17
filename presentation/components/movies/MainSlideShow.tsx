376ae29ba3e97be3b84bce83629d22921ec5b738
Milton Munoz <livemilton@gmail.com>
2026-08-17 16:03:29 -0500

    feat((HU-03)-HU(HU-03): :sparkles: Movies App - React Native - Display movies poster

--- presentation/components/movies/MainSlideShow.tsx
+++ presentation/components/movies/MainSlideShow.tsx
@@ -2,6 +2,7 @@ import { View, Text, Dimensions, useWindowDimensions } from 'react-native'
 import React, { useRef } from 'react'
 import { Movie } from '@/infrastructure/interfaces/movie.interface';
 import Carousel from 'react-native-reanimated-carousel';
+import MoviePoster from './MoviePoster';
 
 interface Props {
     movies: Movie[];
@@ -18,7 +19,7 @@ const MainSlideShow = ({ movies }: Props) => {
             <Carousel
                 ref={ref}
                 data={movies}
-                renderItem={({ item }: { item: Movie }) => <Text>{item.title}</Text>}
+                renderItem={({ item }: { item: Movie }) => <MoviePoster id={item.id} poster={item.poster} smallPoster={false} />}
                 width={200}
                 height={350}
                 style={{
